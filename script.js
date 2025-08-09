28991f14b28db048a458aed6f46a2b5dee7f2ff6
Kesavan_unique <kesavan709495@gmail.com>
2025-08-09 16:12:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("admissionForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your admission form has been submitted successfully!");
+});
